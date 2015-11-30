ed8efcb3c4b692fee9762667840ab9f17ac6ebfa
Gabriel Nakano <gtoshinakano@gmail.com>
2015-11-30 06:54:51 -0200

    Primeiro Commit App e Server

---
+++ myapp-server/server.js
@@ -0,0 +1,33 @@
+var restify = require('restify');
+var colors  = ['#FA8C19', '#4994BE', '#649D6A', '#AF2C4B', '#79AAA6', '#E6C021', '#75497A'];
+
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+// Inicia o server com RESTify
+var server = restify.createServer();
+
+// Faz teste em Dashboard.
+server.get('/testGET', function(req, res){
+
+  res.send({sendData : 'testGET data'}); // JSON enviado ao client
+
+});
+
+server.get('/getREST', function(req, res){
+
+  var rest = ["GET /api/post - Lists all posts", "POST /api/post - Creates a new Post",
+              "GET /api/post/:id - Returns the data for that specific post with the id of :id",
+              "PUT /api/post/:id - Updates the information for a specific post",
+              "DELETE /api/post/:id - Deletes the specific post" ];
+  res.send(rest);
+
+});
+
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
---
+++ myapp/www/js/controllers.js
@@ -0,0 +1,141 @@
+angular.module('starter.controllers', [])
+
+// Constantes de produção
+// Tive problemas de CORS Cross Origin Resource Sharing com ionic server_response
+// Instalado plugin no Chrome
+// https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi
+.constant('production', {
+  url: 'http://localhost:8080'
+})
+
+.controller('DashCtrl', function($scope, $http, production) {
+
+  // Testador de GET no Servidor, envia HTTP request e traz resposta em formato JSON
+  $scope.server_response = null;
+  $http.get(production.url + '/testGET').then(function(resp) {
+    $scope.server_response = resp.data;
+  }, function(err) {
+    $scope.server_response = err;
+    console.error('ERR', err);
+    // err.status will contain the status code
+  });
+  // Pegar array com métodos REST vindos do Servidor e mostrar com ng-repeat
+  $http.get(production.url + '/getREST').then(function(resp) {
+    $scope.methods= resp.data;
+  }, function(err) {
+    $scope.methods = err;
+    console.error('ERR', err);
+    // err.status will contain the status code
+  });
+
+})
+
+/*
+ * Calendário
+ */
+.controller('CalendarCtrl', function($scope) {
+
+  $scope.day = moment();
+
+})
+
+  // Diretiva para calendário
+
+  .directive("calendar", function() {
+      return {
+          restrict: "E",
+          templateUrl: "templates/tpl-calendar.html",
+          scope: {
+              selected: "="
+          },
+          link: function(scope) {
+              scope.selected = _removeTime(scope.selected || moment());
+              scope.month = scope.selected.clone();
+
+              var start = scope.selected.clone();
+              start.date(1);
+              _removeTime(start.day(0));
+
+              _buildMonth(scope, start, scope.month);
+
+              scope.select = function(day) {
+                  scope.selected = day.date;
+              };
+
+              scope.next = function() {
+                  var next = scope.month.clone();
+                  _removeTime(next.month(next.month()+1)).date(1);
+                  scope.month.month(scope.month.month()+1);
+                  _buildMonth(scope, next, scope.month);
+              };
+
+              scope.previous = function() {
+                  var previous = scope.month.clone();
+                  _removeTime(previous.month(previous.month()-1).date(1));
+                  scope.month.month(scope.month.month()-1);
+                  _buildMonth(scope, previous, scope.month);
+              };
+          }
+      };
+
+      function _removeTime(date) {
+          return date.day(0).hour(0).minute(0).second(0).millisecond(0);
+      }
+
+      function _buildMonth(scope, start, month) {
+          scope.weeks = [];
+          var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
+          while (!done) {
+              scope.weeks.push({ days: _buildWeek(date.clone(), month) });
+              date.add(1, "w");
+              done = count++ > 2 && monthIndex !== date.month();
+              monthIndex = date.month();
+          }
+      }
+
+      function _buildWeek(date, month) {
+          var days = [];
+          for (var i = 0; i < 7; i++) {
+              days.push({
+                  name: date.format("dd").substring(0, 1),
+                  number: date.date(),
+                  isCurrentMonth: date.month() === month.month(),
+                  isToday: date.isSame(new Date(), "day"),
+                  date: date
+              });
+              date = date.clone();
+              date.add(1, "d");
+          }
+          return days;
+      }
+  })
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+  // Montar Calendário
+.controller('CalendarCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
